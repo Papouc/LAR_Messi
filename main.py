f6aff02f7f8ec0cb4b6d08d88142e51ad5a8b3c6
Papouc <39922412+Papouc@users.noreply.github.com>
2025-03-19 18:20:21 +0100

    ball centering + depth camera image

--- main.py
+++ main.py
@@ -10,19 +10,24 @@ from image_processor import ImageProcessor, Image
 import math
 
 LOOP_RATE: int = 10
-TURN_ANGLE: float = (1/18) * math.pi
+TURN_ANGLE: float = (1 / 18) * math.pi
+
+IMAGE_CENTER_X: int = 640 / 2
+X_THRESHOLD: int = 10
+
 
 def main() -> None:
-    turtle: Turtlebot = Turtlebot(rgb=True)
+    turtle: Turtlebot = Turtlebot(rgb=True, pc=True)
     main_loop_rate: Rate = Rate(LOOP_RATE)
 
     visual: Visualizer = Visualizer("WINDOW")
 
-    image_reults: list = []
+    image_results: list = []
     prev_search_results: list = []
 
     sanity_check: bool = False
-    robot_state: str = "GENERAL_SEARCH" # GENERAL_SEARCH, CENTER_BALL, MOVE_RADIUS
+    robot_state: str = "GENERAL_SEARCH"  # GENERAL_SEARCH, CENTER_BALL, GET_RADIUS, MOVE_RADIUS
+    last_radius: float = 0.0
 
     while not turtle.is_shutting_down():
         new_img: Image = turtle.get_rgb_image()
@@ -40,46 +45,84 @@ def main() -> None:
         img_processor.filter_color()
 
         result: SceneInfo = img_processor.segment_scene(draw=True)
-        image_reults.append(result)
+        image_results.append(result)
 
         visual.refresh_image(img_processor.retrieve_image(), center_point=True)
 
-        if len(image_reults) < 5:
+        if len(image_results) < 5:
             continue
 
-        search_engine: SearchEngine = SearchEngine(image_reults)
+        search_engine: SearchEngine = SearchEngine(image_results)
         search_result: str = search_engine.determine_state()
-        prev_search_results.append(search_result)
 
         motor_driver: MotorDriver = MotorDriver(turtle, 0.7, 0.0)
 
         if robot_state == "GENERAL_SEARCH":
+            prev_search_results.append(search_result)
+
             if state_machine_step(sanity_check, prev_search_results, motor_driver):
                 if sanity_check:
                     # succesfully found ball
                     sanity_check = False
-                    print("checked")
-                    prev_search_results = []
-                    exit()
+                    robot_state = "CENTER_BALL"
                 else:
                     # initialize double check
                     sanity_check = True
-        elif robot_state == "SANITY_CHECK":
-            pass
+        elif robot_state == "CENTER_BALL":
+            if center_ball(image_results, motor_driver):
+                robot_state = "GET_RADIUS"
+        elif robot_state == "GET_RADIUS":
+
+            if image_results[-1].has_ball:
+                depth_image: np.ndarray = turtle.get_point_cloud()
+                y_ball_pos: int = image_results[-1].ball_position[1]
+                x_ball_pos: int = image_results[-1].ball_position[0]
+
+                last_radius = 0.0
+                for y in range(y_ball_pos - 3, y_ball_pos + 4):
+                    for x in range(x_ball_pos - 3, x_ball_pos + 4):
+                        last_radius += depth_image[y][x][2]
+
+                last_radius /= 7 ** 2
+                print(last_radius)
+                robot_state = "MOVE_RADIUS"
+        elif robot_state == "MOVE_RADIUS":
+
+            if "BALL_FOUND" in prev_search_results and "PINS_FOUND" in prev_search_results:
+                print(prev_search_results)
+
+                ball_index: int = prev_search_results.index("BALL_FOUND")
+                pins_index: int = prev_search_results.index("PINS_FOUND")
+
+                left: bool = ball_index > pins_index
+
+                if abs(ball_index - pins_index) > 15:
+                    left = not left
+
+                print(ball_index, pins_index, left)
+
+                motor_driver.rotate(math.pi / 2, left)
+                exit()
+            else:
+                # previously found both and got here by accident
+                robot_state = "GENERAL_SEARCH"
+
+            prev_search_results = []
 
         main_loop_rate.sleep()
-        image_reults = []
+        image_results = []
 
 
 def state_machine_step(only_ball: bool, prev_search_results: list, motor_driver: MotorDriver) -> bool:
-
     # purely for being sure that the ball is in the image
+
     if only_ball and prev_search_results[-1] == "BALL_FOUND":
         return True
     elif only_ball:
         motor_driver.rotate(TURN_ANGLE)
         return False
 
+    # TODO: write something for both found :D
     if "BOTH_FOUND" in prev_search_results:
         return True
     elif "BALL_FOUND" in prev_search_results and "PINS_FOUND" in prev_search_results:
@@ -89,15 +132,31 @@ def state_machine_step(only_ball: bool, prev_search_results: list, motor_driver:
         pins_index: int = prev_search_results.index("PINS_FOUND")
 
         if pins_index > ball_first_index:
-            motor_driver.rotation_speed *= -1.0
-            motor_driver.rotate((ball_mid_index - pins_index) * TURN_ANGLE)
-            motor_driver.rotation_speed *= -1.0
-            return True
+            motor_driver.rotate(abs(ball_mid_index - pins_index) * TURN_ANGLE, False)
+
+        return True
 
     else:
         motor_driver.rotate(TURN_ANGLE)
 
     return False
+
+
+def center_ball(image_results: list, motor_driver: MotorDriver) -> bool:
+    if image_results[-1].has_ball:
+        # only take this image
+        dist_from_mid: int = image_results[-1].ball_position[0] - IMAGE_CENTER_X
+        if abs(dist_from_mid) < X_THRESHOLD:
+            # ball is ~ centered :D, stop rotation
+            motor_driver.rotate_non_blocking(False, False)
+            return True
+        elif dist_from_mid > 0:
+            motor_driver.rotate_non_blocking(True, False)
+        else:
+            motor_driver.rotate_non_blocking(True, True)
+    else:
+        return False
+
 
 if __name__ == "__main__":
     main()
--- motor_driver.py
+++ motor_driver.py
@@ -1,21 +1,27 @@
 import turtle
+import math
 
 from robolab_turtlebot import Turtlebot, Rate
 from time import sleep
 
 
 class MotorDriver:
-    def __init__(self, turtle: Turtlebot, rotation_speed: float, forward_speed: float):
+    def __init__(self, turtle: Turtlebot, rotation_speed: float, forward_speed: float) -> None:
         self.rotation_speed: float = rotation_speed
         self.forward_speed: float = forward_speed
         self._turtle: Turtlebot = turtle
         self._rate: Rate = Rate(10)
 
-    def rotate(self, degrees: int):
-
+    def rotate(self, degrees: int, left: bool = True) -> None:
         while (abs(self._turtle.get_odometry()[0]) > 0.1 or abs(self._turtle.get_odometry()[1]) > 0.1 or abs(
                 self._turtle.get_odometry()[2]) > 0.1) and (not self._turtle.is_shutting_down()):
             self._turtle.reset_odometry()
+
+        if degrees > math.pi:
+            degrees = abs((2*math.pi - degrees) - (math.pi / 9))
+            left = not left
+
+        self._set_direction(left)
 
         while abs(self._turtle.get_odometry()[2]) < abs(degrees) and (not self._turtle.is_shutting_down()):
             self._turtle.cmd_velocity(linear=0.0, angular=self.rotation_speed)
@@ -23,3 +29,14 @@ class MotorDriver:
 
         self._turtle.cmd_velocity(linear=0.0, angular=0.0)
         self._turtle.reset_odometry()
+
+    def _set_direction(self, left: bool) -> None:
+        self.rotation_speed = abs(self.rotation_speed) if left else -abs(self.rotation_speed)
+
+    def rotate_non_blocking(self, enabled: bool, left: bool) -> None:
+        if not enabled:
+            self._turtle.cmd_velocity(linear=0.0, angular=0.0)
+            return
+
+        self._set_direction(left)
+        self._turtle.cmd_velocity(linear=0.0, angular=self.rotation_speed / 2)
